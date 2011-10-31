08398461beebedb932819dab23b9c89874a6d4fc
Graham Haworth <gdhaworth@gmail.com>
2011-10-31 01:06:58 -0500

    Added a CPU frequency monitor.

--- system-monitor-applet-config.py
+++ system-monitor-applet-config.py
@@ -174,7 +174,7 @@ class SettingFrame:
 
 class App:
     opt = {}
-    setting_items = ('cpu', 'memory', 'swap', 'net', 'disk')
+    setting_items = ('cpu', 'freq', 'memory', 'swap', 'net', 'disk')
 
     def __init__(self):
         self.schema = Gio.Settings('org.gnome.shell.extensions.system-monitor')

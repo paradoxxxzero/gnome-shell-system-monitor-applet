ae8302ffc21904e71700881cf10dfb8577b96a7e
darkxst <tim@feathertop.org>
2011-12-07 09:17:06 +1100

    add another exception

--- system-monitor@paradoxxx.zero.gmail.com/extension.js
+++ system-monitor@paradoxxx.zero.gmail.com/extension.js
@@ -600,10 +600,15 @@ var init = function (metadata) {
             this.update_units();
             Schema.connect('changed::' + this.elt + '-speed-in-bits', Lang.bind(this, this.update_units));
             
-            let iface_list = this.client.get_devices();
-            this.NMsigID = []
-            for(let j = 0; j < iface_list.length; j++){
-            	this.NMsigID[j] = iface_list[j].connect('state-changed' , Lang.bind(this, this.update_iface_list));
+            try {
+                let iface_list = this.client.get_devices();
+                this.NMsigID = []
+                for(let j = 0; j < iface_list.length; j++){
+            	    this.NMsigID[j] = iface_list[j].connect('state-changed' , Lang.bind(this, this.update_iface_list));
+                }
+            }
+            catch(e) {
+                global.logError("Please install Network Manager Gobject Introspection Bindings");
             }
             this.update();
         },

b7001040c0fd26b2e922c51f4c59f3583bcee727
darkxst <tim@feathertop.org>
2012-01-04 14:46:41 +1100

    remove tipBox hover since it crashes shell and is inconsistent with normal shell behaviour anyway

--- system-monitor@paradoxxx.zero.gmail.com/extension.js
+++ system-monitor@paradoxxx.zero.gmail.com/extension.js
@@ -203,16 +203,14 @@ var init = function (metadata) {
 
     TipBox.prototype = {
         _init: function() {
-            this.actor = new St.BoxLayout({ reactive: true });
+            this.actor = new St.BoxLayout({ reactive: true});
             this.actor._delegate = this;
             this.tipmenu = new TipMenu(this.actor);
             // Main.chrome.addActor(this.tipmenu.actor, { visibleInOverview: true,
             // affectsStruts: false });
             this.tipmenu.close();
             this.in_to = this.out_to = 0;
-            // This crash the shell right now
-            // this.actor.connect('enter-event', Lang.bind(this, this.on_enter));
-            // this.actor.connect('leave-event', Lang.bind(this, this.on_leave));
+            
         },
         show_tip: function() {
             this.tipmenu.open();
@@ -231,24 +229,6 @@ var init = function (metadata) {
                 Mainloop.source_remove(this.in_to);
                 this.in_to = 0;
             }
-        },
-        on_enter: function() {
-            if (this.out_to) {
-                Mainloop.source_remove(this.out_to);
-                this.out_to = 0;
-            }
-            if (!this.in_to)
-                this.in_to = Mainloop.timeout_add(500, Lang.bind(this,
-                                                                 this.show_tip));
-        },
-        on_leave: function() {
-            if (this.in_to) {
-                Mainloop.source_remove(this.in_to);
-                this.in_to = 0;
-            }
-            if (!this.out_to)
-                this.out_to = Mainloop.timeout_add(500, Lang.bind(this,
-                                                                  this.hide_tip));
         },
         destroy: function() {
             if (this.in_to) {

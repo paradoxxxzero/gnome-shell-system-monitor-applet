afbc41744ad8e9324b332c40564ba91cbaced91e
darkxst <tim@feathertop.org>
2012-01-06 08:04:26 +1100

    update files

--- po/compile.sh
+++ po/compile.sh
@@ -4,6 +4,6 @@ DEST=$1
 for p in */ 
 do
    echo $p
-   `mkdir -p $1/$p/LC_MESSAGE`
-   `msgfmt ./$p/system-monitor-applet.po -o $1/$p/LC_MESSAGE/system-monitor-applet.mo`
+   `mkdir -p $1/$p/LC_MESSAGES`
+   `msgfmt ./$p/system-monitor-applet.po -o $1/$p/LC_MESSAGES/system-monitor-applet.mo`
 done

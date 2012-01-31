eab3529c5ff0e4ccc2ebcd5db95188eb706f5bf3
darkxst <tim@feathertop.org>
2012-02-01 09:16:16 +1100

    fix internet key issues

--- system-monitor@paradoxxx.zero.gmail.com/extension.js
+++ system-monitor@paradoxxx.zero.gmail.com/extension.js
@@ -643,8 +643,8 @@ var init = function (metadata) {
                 let iface_list = this.client.get_devices();
                 for(let j = 0; j < iface_list.length; j++){
                     if (iface_list[j].state == NetworkManager.DeviceState.ACTIVATED){
-                       this.ifs.push(iface_list[j].get_iface());           
-                    }             
+                       this.ifs.push(iface_list[j].get_ip_iface());
+                    }
                 }
             }
             catch(e) {

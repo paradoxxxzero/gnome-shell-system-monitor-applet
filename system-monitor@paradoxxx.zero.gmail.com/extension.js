2d71821e67488f8415d01896e37412ed365ded95
Tim Lunn <tim@feathertop.org>
2012-09-13 12:38:49 +1000

    let in wrong spot.

--- system-monitor@paradoxxx.zero.gmail.com/extension.js
+++ system-monitor@paradoxxx.zero.gmail.com/extension.js
@@ -1636,10 +1636,11 @@ var enable = function () {
 
         if (Schema.get_boolean("center-display")) {
             if (Schema.get_boolean("move-clock")) {
+                let dateMenu
                 if (shell_Version >= "3.5.91")
-                    let dateMenu = Main.panel.statusArea.dateMenu;
+                    dateMenu = Main.panel.statusArea.dateMenu;
                 else
-                    let dateMenu = Main.panel._dateMenu;
+                    dateMenu = Main.panel._dateMenu;
                 Main.panel._centerBox.remove_actor(dateMenu.actor);
                 Main.panel._rightBox.insert_child_at_index(dateMenu.actor, -1);
             }

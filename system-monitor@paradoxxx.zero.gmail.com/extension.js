c1af5103e86d4a0d03421c58f32efab689818b9c
Chris Spencer <chrisspen@gmail.com>
2019-05-03 00:10:24 -0400

    Reverted ByteArray usage breaking display of thermal and fan speed.

--- system-monitor@paradoxxx.zero.gmail.com/extension.js
+++ system-monitor@paradoxxx.zero.gmail.com/extension.js
@@ -1961,7 +1961,7 @@ const Thermal = class SystemMonitor_Thermal extends ElementBase {
             let file = Gio.file_new_for_path(sfile);
             file.load_contents_async(null, (source, result) => {
                 let as_r = source.load_contents_finish(result)
-                this.temperature = Math.round(parseInt(ByteArray.toString(as_r[1])) / 1000);
+                this.temperature = Math.round(parseInt(as_r[1]) / 1000);
                 if (this.fahrenheit_unit) {
                     this.temperature = Math.round(this.temperature * 1.8 + 32);
                 }
@@ -2032,7 +2032,7 @@ const Fan = class SystemMonitor_Fan extends ElementBase {
             let file = Gio.file_new_for_path(sfile);
             file.load_contents_async(null, (source, result) => {
                 let as_r = source.load_contents_finish(result)
-                this.rpm = parseInt(ByteArray.toString(as_r[1]));
+                this.rpm = parseInt(as_r[1]);
             });
         } else if (this.display_error) {
             global.logError('error reading: ' + sfile);

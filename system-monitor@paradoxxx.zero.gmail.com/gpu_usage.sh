be5652e83131a4fa1ab5f72a707330d2031f3227
franglais125 <franglais125@gmail.com>
2020-04-13 19:14:27 -0400

    Fix wrong extension name in source file: gpu_usage.sh.

--- system-monitor@paradoxxx.zero.gmail.com/gpu_usage.sh
+++ system-monitor@paradoxxx.zero.gmail.com/gpu_usage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ##################################################################################
 #    This file is part of System Monitor Gnome extension.
-#    Apt Update Indicator is free software: you can redistribute it and/or modify
+#    System Monitor Gnome extension is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
-#    Apt Update Indicator is distributed in the hope that it will be useful,
+#    System Monitor Gnome extension is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.

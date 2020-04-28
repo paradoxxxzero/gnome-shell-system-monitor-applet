4ca219e07e144f91495c7b12c60311bfb58437fb
Joakim Roubert <joakim.roubert@gmail.com>
2020-04-28 09:00:50 +0200

    gpu_usage.sh: Replace cut and gawk with shell internals
    The use of cut and gawk in the script can easily be replaced with basic
    POSIX shell script internal parameter expansion functionality. The
    benefit of this is portability, since there are no longer dependencies
    on cut and gawk. (And as a bonus, execution of the script gets more
    lightweight.)
    
    Signed-off-by: Joakim Roubert <joakim.roubert@gmail.com>

--- system-monitor@paradoxxx.zero.gmail.com/gpu_usage.sh
+++ system-monitor@paradoxxx.zero.gmail.com/gpu_usage.sh
@@ -30,9 +30,13 @@ checkcommand()
 if checkcommand nvidia-smi; then
 	nvidia-smi -i 0 --query-gpu=memory.total,memory.used,utilization.gpu --format=csv,noheader,nounits | while IFS=', ' read -r a b c; do echo "$a"; echo "$b"; echo "$c"; done
 elif checkcommand glxinfo; then
-	TOTALVRAM=$(glxinfo | grep -A2 -i GL_NVX_gpu_memory_info | grep -E -i "dedicated" | cut -f2- -d ':' | gawk '{print $1}')
-	AVAILVRAM=$(glxinfo | grep -A4 -i GL_NVX_gpu_memory_info | grep -E -i "available dedicated" | cut -f2- -d ':' | gawk '{print $1}')
-	FREEVRAM=$((TOTALVRAM-AVAILVRAM))
+	TOTALVRAM=$(glxinfo | grep -A2 -i GL_NVX_gpu_memory_info | grep -E -i 'dedicated')
+	TOTALVRAM=${TOTALVRAM##*:[[:blank:]]}
+	TOTALVRAM=${TOTALVRAM%%[[:blank:]]MB*}
+	AVAILVRAM=$(glxinfo | grep -A4 -i GL_NVX_gpu_memory_info | grep -E -i 'available dedicated')
+	AVAILVRAM=${AVAILVRAM##*:[[:blank:]]}
+	AVAILVRAM=${AVAILVRAM%%[[:blank:]]MB*}
+	let FREEVRAM=TOTALVRAM-AVAILVRAM
 	echo "$TOTALVRAM"
 	echo "$FREEVRAM"
 fi
